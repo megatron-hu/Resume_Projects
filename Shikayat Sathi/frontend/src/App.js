b8fa529d8154f549cc3d502a46c0615dc6173600
Lukesh Tiwari <76518032+megatron-hu@users.noreply.github.com>
2024-05-21 18:51:11 +0530

    Add files via upload

---
+++ Shikayat Sathi/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import FIRForm from "./components/FIRForm";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Shikayat Sathi</h1>
+      </header>
+      <main>
+        <FIRForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
